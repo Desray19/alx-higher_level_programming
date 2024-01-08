9218ab1d2b76aaeb10fb7ae95bc32b3e256d669e
Fares <faresyigeremu@gmail.com>
2024-01-08 13:08:53 -0800

    mandatory

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-"""
-modlue with class for comparison
-"""
+"""Defines an empty class BaseGeometry."""
 
 
-def inherits_from(obj, a_class):
-    """
-    returns if an objec is one that inherits
-    from a class
-    """
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
