81b6465eb9a95334212ac8c88e09bef25f319f56
Fares <faresyigeremu@gmail.com>
2024-03-11 13:28:21 -0700

    come on

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const arg = process.argv.length;
-
-if (arg === 2) {
-	console.log('No argument');
-} else if(arg === 3) {
-	console.log('Argument found');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
