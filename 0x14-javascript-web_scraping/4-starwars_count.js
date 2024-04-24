9bb89b0a973f8c6a0139242d42a33d5aa2b3a64d
Fares <faresyigeremu@gmail.com>
2024-04-24 13:43:05 -0700

    semistandard

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    console.log(JSON.parse(body).results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
